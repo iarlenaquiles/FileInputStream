ac708eeb045401c59e2b5a580d0b09f68d60f17b
Iarlen <iarlensilva775@gmail.com>
2017-08-22 16:22:23 -0300

    Criando uma leitura de arquivo

--- src/FileInputStream/Main.java
+++ src/FileInputStream/Main.java
@@ -8,10 +8,12 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		InputStream is = new FileInputStream("arquivo.txt");
-		char b = (char) is.read();
-		System.out.println(b);
-		b = (char) is.read();
-		System.out.println(b);
+		int b;
+		while((b = is.read()) != -1){
+			System.out.print((char)b);
+		}
+		
+		is.close();
 	}
 
 }
